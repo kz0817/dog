2601030f38b0c69bf5178bef4877a7ff9dabd0ef
Kazuhiro Yamato <kz0817@gmail.com>
2019-12-22 09:52:41 +0900

    Updated.

---
+++ doc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='A tool to list processes.')
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
